604062faa7b579370306a1801b7a5844da69b2e0
Marko Cekic <cekicm021@gmail.com>
2020-04-07 13:28:22 +0200

    prod path file

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
-if (process.env.ENV_NODE === "production") {
-  app.use(express.static("client/build"));
-
+if (process.env.NODE_ENV === "production") {
   const path = require("path");
+
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 }
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
